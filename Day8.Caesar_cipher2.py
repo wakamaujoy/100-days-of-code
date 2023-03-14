9de1b61ba933a633366a8f96fa21f437bca69d6f
wakamaujoy <wakamaujoy@gmail.com>
2023-03-14 11:21:02 +0300

    Day8.Complete Caesar cipher

--- Day8.Caesar_cipher2.py
+++ Day8.Caesar_cipher2.py
@@ -1,22 +1,56 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
 
 def caesar(text,shift,direction):
+    shift = shift % 26
     end_text=""
     for letter in text:
-        position = alphabet.index(letter)
-        if direction =="encode":
-            new_position = position + shift
-            new_letter = alphabet[new_position]
-            end_text += new_letter
-        elif direction == "decode":
-            new_position =position - shift
-            new_letter = alphabet[new_position]
-            end_text +=new_letter
+        if letter in alphabet:
+            if direction =="encode":
+                position = alphabet.index(letter)
+                new_position = position + shift
+                new_letter = alphabet[new_position]
+                end_text += new_letter
+            elif direction == "decode":
+                position = alphabet.index(letter)
+                new_position =position - shift
+                new_letter = alphabet[new_position]
+                end_text +=new_letter
+            elif direction != "encode" and direction !="decode":
+                end_of_game = True
+                print("error, try again")
+        else:
+            end_text +=letter
 
     print(f"Your {direction}d message is {end_text}")
+end_of_game = False
+while end_of_game == False:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-caesar(text,shift,direction)
+    caesar(text,shift,direction)
+
+    state = input("Do you want to go again? Type yes or no \n").lower()
+    if state == "no":
+        end_of_game = True
+        print("BYE")
+
+
