ce76bc986476584aa32218fb26df244629e5cd6a
wakamaujoy <wakamaujoy@gmail.com>
2023-03-15 20:21:43 +0300

    complete auction

--- Day9.Secret_Auction.py
+++ Day9.Secret_Auction.py
@@ -15,16 +15,22 @@ print(logo)
 bidding_dictionary = {}
 
 def winner():
-   # bidding_dictionary[name]
-    prize = max(bidding_dictionary[name])
-    print(prize)
-    print(f"The winner of the bid is {name} with {bid}")
+    #print(bidding_dictionary)
+    highest_bid = 0
+    winner =""
+
+    for most in bidding_dictionary:
+        prize = bidding_dictionary[most]
+        if prize > highest_bid:
+            highest_bid = prize
+            winner = most
+    print(f"The winner of the bid is {winner} with {highest_bid}")
 
 end_of_bid = False
 while end_of_bid == False:
     name = input("Write your name?\n")
-    bid = input("Enter your bid price\n")
-    bidding_dictionary[name] = bid
+    bid = int(input("Enter your bid price\n"))
+    bidding_dictionary[name] = (bid)
 
     addition = input("Is there another bidder in the house? type Yes or No\n").lower()
     if addition == "no":
