68fd1d944754fd07415ca7ffe80c8d53f2f0ffd4
wakamaujoy <wakamaujoy@gmail.com>
2023-03-23 00:24:01 +0300

    Day11.Complete_blackjack '

--- Day11.CAPSTONE1_BLACKJACK.py
+++ Day11.CAPSTONE1_BLACKJACK.py
@@ -9,21 +9,15 @@ def deal_card():
 
 def calculate_score(user_cards, computer_cards):
     if sum(user_cards) == 21 or sum(computer_cards) == 21 and range(2):
-        no = 0
-        return no
-
+        return 0
     if 11 in user_cards and sum(user_cards) > 21:
         user_cards.remove(11)
         user_cards.append(1)
-        user_sum = sum(user_cards)
-        return user_sum
-
+        return sum(user_cards)
     if 11 in computer_cards and sum(computer_cards) > 21:
         computer_cards.remove(11)
         computer_cards.append(1)
-        computer_sum = sum(computer_cards)
-        return computer_sum
-
+        return sum(computer_cards)
 
 def play_game():
     deal_card()
@@ -48,20 +42,26 @@ def play_game():
         else:
             if_continues = input("Do you want to draw another card? type y or n")
             if if_continues == "n":
-                game_over = True
-                print("Game Over!")
-
+                if sum(computer_cards) < 17:
+                    computer_cards.append(deal_card())
+                    calculate_score(user_cards,computer_cards)
+                    if sum(computer_cards) > 21:
+                        print("Computer loses")
             else:
                 user_cards.append(deal_card())
                 print(user_cards)
                 print(computer_cards)
                 calculate_score(user_cards, computer_cards)
 
+
+
 play_game()
+
 
 # #Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
 #
-# #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+# #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both
+# have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 #
 # #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
 #
