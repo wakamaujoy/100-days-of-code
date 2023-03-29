772f6e959cee16335e3107740d47faa8a2b35722
wakamaujoy <wakamaujoy@gmail.com>
2023-03-29 17:14:40 +0300

    Coffee_Machine

--- Day15.OOP.Coffee_Machine/main.py
+++ Day15.OOP.Coffee_Machine/main.py
@@ -1,18 +1,31 @@
-from data import resources
-from data import MENU
+from data import resources, MENU
 
 
-def check_resources_sufficient(drink, drink["ingredients"]):
-    for item in drink["ingredients"]:
-        ingredients[item] >= resources[item]
-        print(f"sorry, there is not enough {item}")
-        return False
+def check_resources_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"sorry, there is not enough {item}")
+            return False
     return True
-def process_coins(quarters, dimes, nickles, pennies):
+
+
+def process_coins():
+    print("Please insert coins")
+    quarters = int(input("add quarters"))
+    dimes = int(input("add dimes"))
+    nickles = int(input("add nickles"))
+    pennies = int(input("add pennies"))
     money_in = round((quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01), 2)
+    return money_in
+def check_transaction_successful(paid, cost):
+    if paid < cost:
+        print('Sorry, the amount is not sufficient')
+        return False
+    elif paid >= cost:
+        return True
 
 
-profit =0
+profit = 0
 should_continue = True
 while should_continue:
     choice = input("What would you like, Espresso, latte or cappuccino: ")
@@ -26,16 +39,16 @@ while should_continue:
         print(f"milk = {all_milk}ml")
         print(f"coffee = {all_coffee}g")
         print(f"money = ${profit}")
-
-
     else:
         drink = MENU[choice]
-        check_resources_sufficient(drink["ingredients"])
         if check_resources_sufficient(drink["ingredients"]):
-            quarters = input("add quarters")
-            dimes = input("add dimes")
-            nickles = input("add nickles")
-            pennies = input("add pennies")
-            process_coins()
+            paid = process_coins()
+            if check_transaction_successful(paid, drink["cost"]):
+                balance = paid - drink["cost"]
+                profit += drink["cost"]
+                for item in resources:
+                    resources[item] -= drink["ingredients"][item]
 
-        print(drink)
+
+            print(f"Here is your balance {balance}, enjoy your {choice}")
+
