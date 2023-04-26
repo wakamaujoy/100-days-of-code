06a9a90164c9173532f594d97c8cefea8d749cab
wakamaujoy <wakamaujoy@gmail.com>
2023-04-26 09:24:01 +0300

    Rando_Facts_Qiuz_Game

--- Quiz_Game/main.py
+++ Quiz_Game/main.py
@@ -2,6 +2,8 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 question_bank = []
+
+
 for each_question in question_data:
     q_text = each_question["text"]
     q_ans = each_question["answer"]
