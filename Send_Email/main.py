34a3405e39ce34a05cb265d28d1559856cb579a9
wakamaujoy <wakamaujoy@gmail.com>
2023-05-01 19:23:38 +0300

    Automated_Emails

---
+++ Send_Email/main.py
@@ -0,0 +1,29 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email = "userjoycee@gmail.com"
+my_password ="wetkpklfvpwmagcd"
+
+now = dt.datetime.now()
+weekday =now.weekday()
+
+if weekday == 0:
+    with open("quotes.txt", "rb")as quotes_file:
+        my_quote = quotes_file.readlines()
+        selected_quote = random.choice(my_quote)
+        print(type(selected_quote))
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr= my_email,
+                            to_addrs= "wakamaujoy@gmail.com",
+                            msg= f"Subject:Monday Motivation\n\n{selected_quote}"
+
+        )
+
+
+
+
+
